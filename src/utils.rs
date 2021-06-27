d57f4bbf3a8636bd5bd9208290cb0547d29dd9f5
Amanda Zhu <amandazhuyilan@gmail.com>
2021-06-27 22:24:28 +0900

    add Actor::spawn() function

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,3 @@
 use crate::actor;
 
 pub struct Vector2d(pub f64, pub f64);
-pub struct Frame {
-    pub actors: Vec<actor::Actor>,
-    pub timestamp: u64,
-}
---
+++ src/world.rs
@@ -0,0 +1,9 @@
+pub struct Frame {
+    pub num_actors: usize,
+    pub timestamp: usize,
+}
+
+pub struct World {
+    pub total_num_actor: usize,
+    pub sim_time: usize,
+}
